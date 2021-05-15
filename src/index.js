5927521aff3e6f1dfb430360fe0e7d6a624a9b6d
Arnau <arnau@arnaumarch.com>
2021-05-15 17:56:19 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+export { Button } from "./Button";
+export { Header } from "./Header";
+export {
+  Grid,
+  Content,
+  FixedWrapper,
+  MainWrapper,
+  AppWrapper,
+  Column,
+  Row,
+} from "./Layout";
+export { Thumbnail } from "./Thumbnail";
+export { Progress, Rate } from "./Progress";
+export { H1, H2, H3, P, Label } from "./Typography";
