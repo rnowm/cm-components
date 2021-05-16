f34bac00af6ed16bc2f0b2f9c830a1d8c8361cf9
Arnau <arnau@arnaumarch.com>
2021-05-16 17:12:39 +0200

    Updates

---
+++ precache-manifest.cbe0a37c24cdde13e0abf03e42e16b53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d5134fa081ec5928f869a46bde5d17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5c4a16174f83c027522",
+    "url": "./static/css/main.b779a4c3.chunk.css"
+  },
+  {
+    "revision": "1a9fbaa6360b36dc2a94",
+    "url": "./static/js/2.0a3dcb90.chunk.js"
+  },
+  {
+    "revision": "4de260d6564ec0f9f9fe04c4c7432c78",
+    "url": "./static/js/2.0a3dcb90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c4a16174f83c027522",
+    "url": "./static/js/main.4da9746f.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.4da9746f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84faad436424a2497b2",
+    "url": "./static/js/runtime-main.78b39280.js"
+  },
+  {
+    "revision": "e4eaa44f3bf943629ad52b031ba15d6f",
+    "url": "./static/media/arrow-left.e4eaa44f.svg"
+  }
+]);
